e79a009a3fc9278523d1f3d2da8e1932fbd96ff6
Aditya Raj <adityaraj34@icloud.com>
2026-05-11 23:00:51 +0530

    done with auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import Login from './pages/auth/Login'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
 const App = () => {
   return (
-    <div>
-      <Login />
-      
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
-
+export default App;
